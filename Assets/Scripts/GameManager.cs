b5fdcf7f1041bed67c1effa7cc7f4e464be0527b
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-02-26 11:52:31 +0100

    Add move rocket and meter

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject CameraMain;
+    public GameObject Rocket;
+
+    public Text MeterTextMenu;
+
+    private void Start()
+    {
+        MeterTextMenu.text = "Record: " + PlayerPrefs.GetFloat("Meter", 0).ToString() + "mt.";
+    }
+
+    private void Update()
+    {
+        if(CameraMain != null && Rocket != null)
+            CameraMain.transform.position = new Vector3(CameraMain.transform.position.x, Rocket.transform.position.y+1, CameraMain.transform.position.z);
+    }
+}
--- Assets/Scripts/MoveRocket.cs
+++ Assets/Scripts/MoveRocket.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveRocket : MonoBehaviour
 {
     public Rigidbody2D rb;
     static public bool CanImpulse;
 
+    public float Meter;
+    public Text MeterText;
+
+    public GameObject Explosion;
+
+    private void Start()
+    {
+        Explosion.SetActive(false);
+    }
+
     void Update()
     {
-        print(CanImpulse);
-        if(CanImpulse == true)
+        if(Meter < transform.position.y)
+        {
+            Meter = transform.position.y;
+            if(MeterText != null)
+            {
+                MeterText.text = Meter.ToString("#.##") + "mt.";
+            }
+        }
+        if (rb.velocity.y < -0.1f && Meter != 0)
+        {
+            PlayerPrefs.SetFloat("Meter", Meter);
+            Explosion.transform.position = transform.position;
+            Explosion.SetActive(true);
+            Destroy(this.gameObject);
+        }
+        
+        if (CanImpulse == true)
         {
             rb.AddForce(Vector2.up * BarSystem.ValueTotal, ForceMode2D.Impulse);
-            print("hey");
             CanImpulse = false;
         }
     }
